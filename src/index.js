0680ad3a70e13d8c32b0abd071813c6f87a19c87
komkova <komkova.ar@gmail.com>
2023-04-14 11:02:07 +0300

    index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 
@@ -9,7 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Routes>
+      <Route path='/' element={<App />}>
+
+      </Route>
+    </Routes>
+      
     </BrowserRouter>
   </React.StrictMode>
 );
