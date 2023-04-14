05e111385f91d95db86b57001e25f2c0a80446b2
komkova <komkova.ar@gmail.com>
2023-04-14 11:06:38 +0300

    Revert "index.html"
    This reverts commit 0680ad3a70e13d8c32b0abd071813c6f87a19c87.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 
@@ -9,12 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}>
-
-      </Route>
-    </Routes>
-      
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
